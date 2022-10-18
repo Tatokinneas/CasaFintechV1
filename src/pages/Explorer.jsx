10546bf5b9fd630ec03a51debfa463a2aad18c5f
david7ello <david7ello@gmail.com>
2022-10-17 22:00:05 -0500

    feat: explorer

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { MdLocationOn } from "react-icons/md";
 import Maps from "../components/explorer/Maps";
+import { BiBed } from "react-icons/bi";
+import { TbBath } from "react-icons/tb";
+import { GiHomeGarage } from "react-icons/gi";
+import {FiHeart} from "react-icons/fi";
+import {AiFillFire,AiOutlineClockCircle} from "react-icons/ai"
 
 const Explorer = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -82,224 +87,224 @@ const Explorer = () => {
     <div className="grid grid-cols-1 lg:flex m-5">
       <div className="card w-full h-[625px] lg:w-96 mr-5 my-5 bg-gray-200 shadow-xl  lg:sticky top-5  ">
         <div className="overflow-auto">
-        <div className="card-body p-4">
-          <h2 className="card-title font-bold text-2xl flex justify-center space-x-3">
-            <MdLocationOn /> LOCALIDAD
-          </h2>
-        </div>
-        <Maps />
-        <div className="form-control px-5">
-          <label className="label py-0">
-            <span className="label-text font-bold ">RANGO DE PRECIO</span>
-          </label>
-          <input
-            type="range"
-            min="1"
-            max="6"
-            className="range range-accent "
-            onChange={rangoPrecio}
-          />
-          <label className="label font-semibold">
-            <span className="label-text-alt">Minimo</span>
-            <span className="label-text-alt">Maximo</span>
-          </label>
-        </div>
-        <div className="form-control px-5 mt-4">
-          <label className="label py-0">
-            <span className="label-text font-bold ">SUPERFICIE</span>
-          </label>
-          <input
-            type="range"
-            min="1"
-            max="6"
-            className="range range-accent"
-            onChange={rangoSuperficie}
-          />
-          <label className="label font-semibold">
-            <span className="label-text-alt">Minimo</span>
-            <span className="label-text-alt">Maximo</span>
-          </label>
-        </div>
-        <h1 className="ml-5 mt-3 font-bold">CARACTERÍSTICAS</h1>
-        <div className="mx-5 space-y-3 font-semibold text-sm mt-5">
-          <div className="flex justify-between">
-            <h1>RECAMARAS</h1>
-            <input type="number" className="w-16 flex justify-center" />
+          <div className="card-body p-4">
+            <h2 className="card-title font-bold text-2xl flex justify-center space-x-3">
+              <MdLocationOn /> LOCALIDAD
+            </h2>
           </div>
-          <div className="flex justify-between">
-            <h1>BAÑOS COMPLETOS</h1>
-            <input type="number" className="w-16 flex justify-center" />
+          <Maps />
+          <div className="form-control px-5">
+            <label className="label py-0">
+              <span className="label-text font-bold ">RANGO DE PRECIO</span>
+            </label>
+            <input
+              type="range"
+              min="1"
+              max="6"
+              className="range range-accent "
+              onChange={rangoPrecio}
+            />
+            <label className="label font-semibold">
+              <span className="label-text-alt">Minimo</span>
+              <span className="label-text-alt">Maximo</span>
+            </label>
           </div>
-          <div className="flex justify-between">
-            <h1>ESTACIONAMIENTO</h1>
-            <input type="number" className="w-16 flex justify-center" />
+          <div className="form-control px-5 mt-4">
+            <label className="label py-0">
+              <span className="label-text font-bold ">SUPERFICIE</span>
+            </label>
+            <input
+              type="range"
+              min="1"
+              max="6"
+              className="range range-accent"
+              onChange={rangoSuperficie}
+            />
+            <label className="label font-semibold">
+              <span className="label-text-alt">Minimo</span>
+              <span className="label-text-alt">Maximo</span>
+            </label>
           </div>
-          <div className="flex justify-between">
-            <h1>BALCÓN</h1>
-            <input type="number" className="w-16 flex justify-center" />
+          <h1 className="ml-5 mt-3 font-bold">CARACTERÍSTICAS</h1>
+          <div className="mx-5 space-y-3 font-semibold text-sm mt-5">
+            <div className="flex justify-between">
+              <h1>RECAMARAS</h1>
+              <input type="number" className="w-16 flex justify-center" />
+            </div>
+            <div className="flex justify-between">
+              <h1>BAÑOS COMPLETOS</h1>
+              <input type="number" className="w-16 flex justify-center" />
+            </div>
+            <div className="flex justify-between">
+              <h1>ESTACIONAMIENTO</h1>
+              <input type="number" className="w-16 flex justify-center" />
+            </div>
+            <div className="flex justify-between">
+              <h1>BALCÓN</h1>
+              <input type="number" className="w-16 flex justify-center" />
+            </div>
+            <div className="flex justify-between">
+              <h1>PET FRIENDLY</h1>
+              <select className=" rounded-none  select-xs w-16 max-w-xs">
+                <option>SI</option>
+                <option>NO</option>
+                <option>SOLO GATOS</option>
+              </select>
+            </div>
           </div>
-          <div className="flex justify-between">
-            <h1>PET FRIENDLY</h1>
-            <select className=" rounded-none  select-xs w-16 max-w-xs">
-              <option>SI</option>
-              <option>NO</option>
-              <option>SOLO GATOS</option>
-            </select>
-          </div>
-        </div>
-        <h1 className="ml-5 mt-6 font-bold">AMENIDADES</h1>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>GIMNASIO</h1>
-            {check1 ? (
-              <input
-                type="checkbox"
-                checked="checked"
-                className="checkbox checkbox-accent "
-                onClick={() => cambioCheckF1()}
-              />
-            ) : (
-              <input
-                type="checkbox"
-                checked=""
-                className="checkbox checkbox-accent "
-                onClick={() => cambioCheck1()}
-              />
-            )}
-          </div>
-        </div>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>LUDOTECA</h1>
-            <div className="form-control">
-              {check2 ? (
+          <h1 className="ml-5 mt-6 font-bold">AMENIDADES</h1>
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>GIMNASIO</h1>
+              {check1 ? (
                 <input
                   type="checkbox"
                   checked="checked"
                   className="checkbox checkbox-accent "
-                  onClick={() => cambioCheckF2()}
+                  onClick={() => cambioCheckF1()}
                 />
               ) : (
                 <input
                   type="checkbox"
                   checked=""
                   className="checkbox checkbox-accent "
-                  onClick={() => cambioCheck2()}
+                  onClick={() => cambioCheck1()}
                 />
               )}
             </div>
           </div>
-        </div>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>ROOF GARDEN</h1>
-            <div className="form-control">
-              {check3 ? (
-                <input
-                  type="checkbox"
-                  checked="checked"
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheckF3()}
-                />
-              ) : (
-                <input
-                  type="checkbox"
-                  checked=""
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheck3()}
-                />
-              )}
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>LUDOTECA</h1>
+              <div className="form-control">
+                {check2 ? (
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheckF2()}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked=""
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheck2()}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>PLAYGROUND</h1>
-            <div className="form-control">
-              {check4 ? (
-                <input
-                  type="checkbox"
-                  checked="checked"
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheckF4()}
-                />
-              ) : (
-                <input
-                  type="checkbox"
-                  checked=""
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheck4()}
-                />
-              )}
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>ROOF GARDEN</h1>
+              <div className="form-control">
+                {check3 ? (
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheckF3()}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked=""
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheck3()}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>ALBERCA</h1>
-            <div className="form-control">
-              {check5 ? (
-                <input
-                  type="checkbox"
-                  checked="checked"
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheckF5()}
-                />
-              ) : (
-                <input
-                  type="checkbox"
-                  checked=""
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheck5()}
-                />
-              )}
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>PLAYGROUND</h1>
+              <div className="form-control">
+                {check4 ? (
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheckF4()}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked=""
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheck4()}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>SALA DE EVENTOS</h1>
-            <div className="form-control">
-              {check6 ? (
-                <input
-                  type="checkbox"
-                  checked="checked"
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheckF6()}
-                />
-              ) : (
-                <input
-                  type="checkbox"
-                  checked=""
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheck6()}
-                />
-              )}
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>ALBERCA</h1>
+              <div className="form-control">
+                {check5 ? (
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheckF5()}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked=""
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheck5()}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-5 font-semibold mx-5 text-sm">
-          <div className="flex justify-between">
-            <h1>CANCHAS</h1>
-            <div className="form-control mb-5">
-              {check7 ? (
-                <input
-                  type="checkbox"
-                  checked="checked"
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheckF7()}
-                />
-              ) : (
-                <input
-                  type="checkbox"
-                  checked=""
-                  className="checkbox checkbox-accent "
-                  onClick={() => cambioCheck7()}
-                />
-              )}
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>SALA DE EVENTOS</h1>
+              <div className="form-control">
+                {check6 ? (
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheckF6()}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked=""
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheck6()}
+                  />
+                )}
+              </div>
             </div>
           </div>
-        </div>
+          <div className="mt-5 font-semibold mx-5 text-sm">
+            <div className="flex justify-between">
+              <h1>CANCHAS</h1>
+              <div className="form-control mb-5">
+                {check7 ? (
+                  <input
+                    type="checkbox"
+                    checked="checked"
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheckF7()}
+                  />
+                ) : (
+                  <input
+                    type="checkbox"
+                    checked=""
+                    className="checkbox checkbox-accent "
+                    onClick={() => cambioCheck7()}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -311,16 +316,46 @@ const Explorer = () => {
                 <figure>
                   <img src={item.fotos} alt="Shoes" className="h-52 w-full" />
                 </figure>
-                <div className="card-body p-5">
+                <div className="flex">
+                <div className="card-body px-5 py-0 mb-5">
                   <div>
                     <h1 className="font-bold">{item.name}</h1>
                   </div>
-                  <div className="text-sm text-gray-700 font-semibold -mt-2">{item.address}</div>
+                  <div className="text-sm text-gray-700 font-semibold -mt-2">
+                    {item.address}
+                  </div>
                   <div className="text-verdes text-2xl font-bold -mt-2">
                     ${item.caract_properties[0].precio}.00
                   </div>
+                  <div className="flex justify-between ">
+                    <div className="flex">
+                      <BiBed className="text-2xl   bg-slate-200 rounded-l-sm" />
+                      <div className="flex justify-center text-lg h-[24px] font-bold px-2  bg-slate-300 rounded-r-sm">
+                        {item.caract_properties[0].recamaras}
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <TbBath className="text-2xl   bg-slate-200 rounded-l-sm" />
+                      <div className="flex justify-center text-lg h-[24px] font-bold px-2  bg-slate-300 rounded-r-sm">
+                        {item.caract_properties[0].baños}
+                      </div>
+                    </div>
+                    <div className="flex">
+                      <GiHomeGarage className="text-2xl   bg-slate-200 rounded-l-sm" />
+                      <div className="flex justify-center text-lg h-[24px] font-bold px-2  bg-slate-300 rounded-r-sm">
+                        {item.caract_properties[0].parqueadero}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="btn-group btn-group-vertical mt-5 mr-2 space-y-3">
+                  <button className=" p-0 bg-transparent hover:bg-transparent border-none "><FiHeart className="text-3xl text-purple-900"/></button>
+                  <button className=" p-0 bg-transparent hover:bg-transparent border-none"><AiFillFire className="text-3xl text-orange-500"/></button>
+                  <button className=" p-0 bg-transparent hover:bg-transparent border-none"><AiOutlineClockCircle className="text-3xl text-verdes"/></button>
+                </div>
                 </div>
               </div>
+              
             </div>
           );
         })}
